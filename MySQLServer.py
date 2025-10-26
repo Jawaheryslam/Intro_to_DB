0b5200bff15fd07d569a07dfd43d9e2820289887
Jawaheryslam <orchidgraphics19@gmail.com>
2025-10-27 02:16:08 +0300

    correction for a python script that creates a database and handles errors

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,7 @@ try:
     connection = mysql.connector.connect(host='localhost')
 
     cursor = connection.cursor()
-    cursor.execute("Create DATABASE IF NOT EXISTS alx_book_store")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 
 except Error as e:
